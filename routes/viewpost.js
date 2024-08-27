ca41292cfb7717dd35cb61001cd0beddc8917a5a
Ethan Cho <ethanwonc@icloud.com>
2024-08-27 20:34:59 +0900

    Applied date formatting at every route.

--- routes/viewpost.js
+++ routes/viewpost.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require("../config/mysql");
 var conn = db.init();
+const moment = require('moment');
 
 router.get('/', function(req, res) {
   if(req.query.postid) {
@@ -13,6 +14,8 @@ router.get('/', function(req, res) {
         console.log("query error: " + err);
         res.send(NULL);
       } else {
+        result[0].written_date = moment(result[0].written_date).format('MMMM D YYYY');
+        console.log(result[0].written_date);
         res.render("viewpost", result[0]);
       }
     });
