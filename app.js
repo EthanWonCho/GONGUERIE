7ac2cb28d45ad99dbe068b37e8382f9fd845e62b
EthanWonCho <realethanwoncho@gmail.com>
2024-09-03 20:20:13 +0900

    POST action

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var announcementsRouter = require('./routes/announcements');
 var viewPostRouter = require('./routes/viewpost');
 var writePostRouter = require('./routes/writepost');
+var writeRouter = require('./routes/write');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/announcements', announcementsRouter);
 app.use('/viewpost', viewPostRouter);
 app.use('/writepost', writePostRouter);
+app.use('/write', writeRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
