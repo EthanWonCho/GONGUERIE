63e4c4c2b9a6885a6fd5844ec1998eff6b2b56d3
EthanWonCho <realethanwoncho@gmail.com>
2024-09-10 18:07:58 +0900

    removed 'next' on line 8

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var db = require("../config/mysql");
 var conn = db.init();
 const helper = require('../helper');
 
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   var cmd = "select * from announcements";
   conn.query(cmd, function(err, result) {
     res.render('index', {res: result, helper: helper});
