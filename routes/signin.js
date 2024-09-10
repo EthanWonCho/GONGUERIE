87db69edafd316f223247c87bd02ad40f2ced40a
EthanWonCho <realethanwoncho@gmail.com>
2024-09-10 17:14:36 +0900

    Add Routes for sign-in and sign-up

---
+++ routes/signin.js
@@ -0,0 +1,12 @@
+var createError = require('http-errors');
+var express = require('express');
+var router = express.Router();
+var db = require("../config/mysql");
+var conn = db.init();
+const helper = require('../helper');
+
+router.get('/', function(req, res) {
+  res.render('signin');
+});
+
+module.exports = router;
