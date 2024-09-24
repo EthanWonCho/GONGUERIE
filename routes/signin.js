3bf0c40d76dbbf93d31630ee77fe259c0f2627a4
Ethan <82100898+EthanWonCho@users.noreply.github.com>
2024-09-24 22:47:11 +0900

    Session (#4)
    * Connect sign up with database.
    Created new table in MySQL named 'user'
    
    * Added signin
    changed the test and start command to include .env file
    changed signup.css filename to auth.css since signin and signup ejs is both using the same.
    
    * Anti-injection
    
    * Encrypt user password on server

--- routes/signin.js
+++ routes/signin.js
@@ -1,12 +1,49 @@
 var createError = require('http-errors');
 var express = require('express');
 var router = express.Router();
+
+const helper = require('../helper');
+const bcrypt = require('bcrypt');
+
 var db = require("../config/mysql");
 var conn = db.init();
-const helper = require('../helper');
 
 router.get('/', function(req, res) {
   res.render('signin');
+});
+
+router.post('/', function(req, res, next) {
+  var cmd = 'SELECT pw from user where id = ?';
+  params = [req.body.id];
+  conn.query(cmd, params, function(err, result) {
+    if(err) {
+      console.error('Query Error: ', err);
+      next(createError(400));
+    } else {
+      bcrypt.compare(req.body.pw, result[0].pw, (err, result) => {
+        if (err) {
+          // Handle error
+          console.error('Error comparing passwords:', err);
+          next(createError(503));
+          return;
+        }
+        if (result) {
+          // Passwords match, authentication successful
+          console.log('Passwords match! User authenticated.');
+          // res.redirect("/");
+          res.status(200).send();
+          // req.session.user = {
+          //   id: req.body.id,
+          //   authorized: true,
+          // };
+        } else {
+          // Passwords don't match, authentication failed
+          console.log('Passwords do not match! Authentication failed.');
+          res.status(400).send();
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
